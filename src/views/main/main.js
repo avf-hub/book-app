cd83727ac626df68184c26d161863e3462484cc9
Andrey Falkovskiy <avf-91@yandex.ru>
2025-12-07 01:19:49 +0300

    Состояние загрузки

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -2,6 +2,7 @@ import {AbstractView} from "../../common/view.js";
 import onChange from "on-change";
 import {Header} from "../../components/header/header.js";
 import {Search} from "../../components/search/search.js";
+import {CardList} from "../../components/card_list/card-list.js";
 
 export class MainView extends AbstractView {
     state = {
@@ -29,8 +30,11 @@ export class MainView extends AbstractView {
         if (path === "searchQuery") {
             this.state.loading = true;
             const data = await this.loadList(this.state.searchQuery, this.state.offset);
+            this.state.loading = false;
             this.state.list = data.docs;
-            this.state.loading = true;
+        }
+        if (path === "list" || path === "loading") {
+            this.render();
         }
     }
 
@@ -42,6 +46,7 @@ export class MainView extends AbstractView {
     render() {
         const main = document.createElement("div");
         main.append(new Search(this.state).render());
+        main.append(new CardList(this.appState, this.state).render());
         this.app.innerHTML = "";
         this.app.append(main);
         this.renderHeader();
